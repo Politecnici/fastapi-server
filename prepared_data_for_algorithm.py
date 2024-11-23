3a1941ac6e844317cadb70d48351a422e5eca1be
ErTucano <pietrobottan@gmail.com>
2024-11-23 16:34:45 +0100

    protocode

--- prepared_data_for_algorithm.py
+++ prepared_data_for_algorithm.py
@@ -4,8 +4,8 @@ import math
 # to reach a certain customer.
 
 # The matrix we will use will have the following shape:
-# m = number of taxis
-# n = number of customers
+# n = number of taxis
+# m = number of customers
 # In the (1,1) position we will have the distance from the first taxi to the first customer
 
 # We will get the position of all the taxis and the customers. To calculate the distance between the empty taxis and the customers
@@ -14,20 +14,17 @@ import math
 # For the occupied taxis it's a bit more complicated. We will calculate the distance from the taxi to where the customer must be dropped, 
 # we will then add the distance from the drop point to the other customers. We will then calculate the time to cover the distance.
 
-taxi_pos = np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
-customer_start_pos = np.array([[11,12],[13,14],[15,16],[17,18],[19,20]])
-
-n_taxi = taxi_pos.shape[0]
-m_customer = customer_start_pos.shape[0]
-
-# We will calculate the distance from the taxi to the customer
-
-distance_matrix = np.zeros((n_taxi,m_customer))
-
-for i in range(n_taxi):
-    for j in range(m_customer):
-        distance_matrix[i,j] = get_distance(taxi_pos[i], customer_start_pos[j])
+taxi_distance # distance of the all the taxis to the customers, we get it from hungarian_algorithm.py 
 
 
+times = taxi_distance/taxi_velocity
 
-# np.asarray([[82,83,69,92],[77,37,49,92],[11,69,5,86],[8,9,98,23]]) example on how the data must be put in the class
+fast_time = min(times) # time to cover the distance
+
+fast_index = #index of the taxi that will reach the customer first
+fast_index = times.index(fast_time)
+
+taxi_distance_to_destination = taxi_distance - taxi_velocity*fast_time
+new_distances = taxi_distance_to_destination + destination_to_new_customer_distance
+
+# Ributto poi in hungarian_algorithm.py che mi da tutte le seconde destinazioni 
